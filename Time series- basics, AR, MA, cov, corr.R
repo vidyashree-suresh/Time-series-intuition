3a453a25ccae777da03532b3ad6de49f2a23f8c1
Vidya shree S <55318342+vidyashree-suresh@users.noreply.github.com>
2020-12-11 19:23:53 +0530

    Update Time series- basics, AR, MA, cov, corr.R

--- Time series- basics, AR, MA, cov, corr.R
+++ Time series- basics, AR, MA, cov, corr.R
@@ -129,4 +129,37 @@ plot(rw_1)
 stats::acf(rw)
 stats::acf(rw,type = "partial")
 
+################################understanding ACF
+
+
+set.seed(2016); N=1000; phi = 1;
+Z = rnorm(N,0,1); X=NULL; X[1] = Z[1];
+for (t in 2:N) {
+  X[t] = Z[t] + phi*X[t-1] ;
+}
+X.ts = ts(X)
+par(mfrow=c(2,1))
+plot(X.ts,main="AR(1) Time Series on White Noise, phi=.4")
+X.acf = acf(X.ts, main="AR(1) Time Series on White Noise, phi=.4")
+
+
+
+set.seed(2017)
+X.ts <- arima.sim(list(ar = c(.7, .2)), n=1000)
+par(mfrow=c(2,1))
+plot(X.ts,main="AR(2) Time Series, phi1=.7, phi2=.2")
+X.acf = acf(X.ts, main="Autocorrelation of AR(2) Time Series")
+
+
+set.seed(2016); N=1000; phi = .4;
+Z = rnorm(N,0,1)
+X=NULL;
+X[1] = Z[1];
+for (t in 2:N) {
+  X[t] = Z[t] + phi*X[t-1] ;
+}
+X.ts = ts(X)
+X.acf = acf(X.ts)
+X.acf$acf
+
 
